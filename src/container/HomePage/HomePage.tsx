6f604bd3d41446ebac6cda2220f9f3d5cbb434c0
Karl Crisostomo <karlmichaelcrisostomo123@gmail.com>
2024-10-30 15:11:05 +0800

    feat: add HomePage

---
+++ src/container/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import { NextPageWithLayout } from "@/types";
+import React from "react";
+
+const HomePage: NextPageWithLayout = () => {
+  return (
+    <div>
+      <span>ForgeBox</span>
+    </div>
+  );
+};
+
+export default HomePage;
