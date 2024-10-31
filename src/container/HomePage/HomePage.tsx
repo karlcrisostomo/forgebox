3c7e9ca5bdf9579e0f000ccf9bd33e0f1105be3c
Karl Crisostomo <karlmichaelcrisostomo123@gmail.com>
2024-10-31 15:02:51 +0800

    test

--- src/container/HomePage/HomePage.tsx
+++ src/container/HomePage/HomePage.tsx
@@ -4,12 +4,11 @@ import React from "react";
 const HomePage: NextPageWithLayout = () => {
   const { increment, decrement, count } = useCounterSlice();
   return (
+    //test
     <div>
       <span>ForgeBox</span>
-
       <h1>Counter</h1>
       <span>{count}</span>
-
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
     </div>
