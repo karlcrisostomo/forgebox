dc735b20fbd4eb2d6e47112c682fa26213afbfd8
Karl Crisostomo <karlmichaelcrisostomo123@gmail.com>
2025-02-02 17:03:08 +0800

    test: add temporary UI for testing color picker integration

--- src/container/HomePage/HomePage.tsx
+++ src/container/HomePage/HomePage.tsx
@@ -1,15 +1,57 @@
 import { NextPageWithLayout } from "@/types";
 import styles from "./styles.module.scss";
 import { RootLayout } from "@/layouts/RootLayout";
-import { DraggableToolBar } from "@/components";
+import { GeneralToolbar } from "@/components";
+import { useThemeChange } from "@/hooks";
+import { useEffect } from "react";
 
 const HomePage: NextPageWithLayout = () => {
+  const { primaryColor, secondaryColor, textColor, accentColor, isDarkMode } =
+    useThemeChange();
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      isDarkMode ? "dark" : "light",
+    );
+  }, [isDarkMode]);
+
   return (
     <div className={styles.container}>
-      <DraggableToolBar toolbarHeight={50} />
+      <div
+        style={{
+          backgroundColor: primaryColor,
+          padding: "2rem",
+          borderRadius: "1rem",
+        }}
+      >
+        <span style={{ color: textColor }}>Primary</span>
+      </div>
+      <div
+        style={{
+          backgroundColor: secondaryColor,
+          padding: "2rem",
+          borderRadius: "1rem",
+        }}
+      >
+        <span style={{ color: textColor }}>secondary</span>
+      </div>
+      <div
+        style={{
+          backgroundColor: accentColor,
+          padding: "2rem",
+          borderRadius: "1rem",
+        }}
+      >
+        <span style={{ color: textColor }}>accent</span>
+      </div>
+
+      <GeneralToolbar />
     </div>
   );
 };
+
+HomePage.displayName = "HomePage";
 
 HomePage.getLayout = (page) => <RootLayout>{page}</RootLayout>;
 
