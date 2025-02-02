3b0344257840d5e373ddd475583a23cc7b3f5490
Karl Crisostomo <karlmichaelcrisostomo123@gmail.com>
2025-02-02 17:02:08 +0800

    refactor: simplify toolbar animation with scale transition

--- src/animations/animatedToolBar.ts
+++ src/animations/animatedToolBar.ts
@@ -1,25 +1,12 @@
 import { Variants } from "framer-motion";
 
-//https://easings.net/#easeOutCubic
-
 export const animateToolBar: Variants = {
   initial: {
     opacity: 0,
-    width: 0,
-    x: -250, //initial x postion of the toolbar component might adjust the x position based on the parent component's position
-    transition: {
-      duration: 0.25,
-      ease: [0.33, 1, 0.68, 1],
-    },
+    scale: 0.5,
   },
   animate: {
     opacity: 1,
-    width: "20%",
-    x: 0,
-    transition: {
-      delay: 0.5,
-      duration: 0.25,
-      ease: [0.33, 1, 0.68, 1],
-    },
+    scale: 1,
   },
 };
