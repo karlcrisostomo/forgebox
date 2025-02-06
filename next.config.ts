abd64dca7c8d7f9e4fb7ea75cbad277ba1341028
Karl Crisostomo <karlmichaelcrisostomo123@gmail.com>
2025-02-06 15:42:33 +0800

    chore(nextjs): update sassOptions to include src directory

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
-  // added a basepath for eg. /forgeBox/Home
-
   publicRuntimeConfig: {
     basePath: "/forgeBox",
   },
   assetPrefix: "/forgeBox",
   reactStrictMode: true,
   sassOptions: {
+    includePaths: [path.join(__dirname, "src")],
     silenceDeprecations: ["legacy-js-api"],
   },
 };
